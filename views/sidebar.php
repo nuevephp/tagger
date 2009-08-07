b6394698f4268396c24bc96c275478920a2ffc3c
dajare <dajare@gmail.com>
2009-08-07 21:24:15 +0800

    conversion from frog to wolf

--- views/sidebar.php
+++ views/sidebar.php
@@ -1,8 +1,8 @@
 <?php if (Dispatcher::getAction() != 'view'): ?>
 
-<p class="button"><a href="<?php echo get_url('plugin/tagger/add'); ?>"><img src="../frog/plugins/tagger/images/tag.png" align="middle" alt="snippet icon" /> <?php echo __('New Tag'); ?></a></p>
-<p class="button"><a href="<?php echo get_url('plugin/tagger/settings'); ?>"><img src="../frog/plugins/tagger/images/settings.png" align="middle" alt="page icon" /> <?php echo __('Settings'); ?></a></p>
-<p class="button"><a href="<?php echo get_url('plugin/tagger/documentation'); ?>"><img src="../frog/plugins/tagger/images/documentation.png" align="middle" alt="snippet icon" /> <?php echo __('Documentation'); ?></a></p>
+<p class="button"><a href="<?php echo get_url('plugin/tagger/add'); ?>"><img src="../wolf/plugins/tagger/images/tag.png" align="middle" alt="snippet icon" /> <?php echo __('New Tag'); ?></a></p>
+<p class="button"><a href="<?php echo get_url('plugin/tagger/settings'); ?>"><img src="../wolf/plugins/tagger/images/settings.png" align="middle" alt="page icon" /> <?php echo __('Settings'); ?></a></p>
+<p class="button"><a href="<?php echo get_url('plugin/tagger/documentation'); ?>"><img src="../wolf/plugins/tagger/images/documentation.png" align="middle" alt="snippet icon" /> <?php echo __('Documentation'); ?></a></p>
 
 <div class="box">
     <h2><?php echo __('What is a Tag?'); ?></h2>
